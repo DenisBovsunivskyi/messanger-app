acf7a1cae408fe74411082a93e66ec67fbc832bf
Denis Bovsunovsky <pi58_bdm@student.ztu.edu.ua>
2021-06-25 01:19:00 +0300

    My messagin app

--- Diplom/app/src/main/java/com/example/telegramdiplom/utilits/Notifications/Constants.kt
+++ Diplom/app/src/main/java/com/example/telegramdiplom/utilits/Notifications/Constants.kt
@@ -3,7 +3,7 @@ package com.example.telegramdiplom.utilits.Notifications
 class Constants {
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAZFqukPE:APA91bFtZIG6keKWdIAmTg5C7blhrBFOEiMMb7FF9GFSlBeGMeolebxFOMwBIM5TIoQ0RCZzGsCgqEybRbWZWfRgr60eO89i2YyeKFD0IRkl_rhkxir_7gvJ4YWPKmIWZkjVFoa4kMxp"
+        const val SERVER_KEY = "\\\"
         const val CONTENT_TYPE = "application/json"
     }
 }
